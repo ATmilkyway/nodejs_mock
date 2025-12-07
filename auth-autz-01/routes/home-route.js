4098128f80d5ce43d1baf03705b3d885e0052bd3
ATmilkyway <125128752+ATmilkyway@users.noreply.github.com>
2025-12-07 22:23:00 +0300

    feat: add home and admin routes

---
+++ auth-autz-01/routes/home-route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to home page",
+  });
+});
+
+module.exports = router;
--- auth-autz-01/server.js
+++ auth-autz-01/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./database/db");
 const authRoute = require("./routes/auth-route");
-
+const homeRoute = require("./routes/home-route");
+const adminRoute = require("./routes/admin-route");
 const app = express();
 
 app.use(express.json());
@@ -12,6 +13,8 @@ connectDB();
 
 // routes
 app.use("/api/", authRoute);
+app.use("/home/", homeRoute);
+app.use("/admin/", adminRoute);
 
 const PORT = process.env.PORT || 4000;
 
