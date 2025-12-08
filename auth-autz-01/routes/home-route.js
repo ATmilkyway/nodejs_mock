ec575fa6623c4b25f268790655cb1185299cde49
ATmilkyway <natnaelasegd0507@example.com>
2025-12-08 14:01:51 +0300

    feat: update home route

--- auth-autz-01/routes/home-route.js
+++ auth-autz-01/routes/home-route.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const authMiddleware = require("../middleware/auth-middleware");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", authMiddleware, (req, res) => {
+  const user = req.userInfo;  
+
   res.status(200).json({
     success: true,
-    message: "Welcome to home page",
+    message: `Welcome to our platform page, ${user.username}`,
+    data: {
+      userId: user._id,
+      username: user.username,
+      role: user.role,
+    },
   });
 });
 
