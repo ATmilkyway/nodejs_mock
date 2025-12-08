b91587ac6e1038a67f1c36c9de44ac443aea6c97
ATmilkyway <natnaelasegd0507@example.com>
2025-12-08 13:53:52 +0300

    feat: return data for atuz user

--- auth-autz-01/routes/admin-route.js
+++ auth-autz-01/routes/admin-route.js
@@ -4,9 +4,19 @@ const authMiddleware = require("../middleware/auth-middleware");
 const router = express.Router();
 
 router.get("/", authMiddleware, (req, res) => {
+  if (!req.userInfo) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized: User information missing",
+    });
+  }
+
+  const { userId, username, role } = req.userInfo;
+
   res.status(200).json({
     success: true,
     message: "Welcome to admin page",
+    data: { userId, username, role },
   });
 });
 
