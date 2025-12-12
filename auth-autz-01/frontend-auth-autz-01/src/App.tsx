d73cb97444faa8fe82decbc725d7ac28157dcb12
ATmilkyway <natnaelasegd0507@gmail.com>
2025-12-12 09:36:47 +0300

    featr: build responsive layout

--- auth-autz-01/frontend-auth-autz-01/src/App.tsx
+++ auth-autz-01/frontend-auth-autz-01/src/App.tsx
@@ -1,7 +1,31 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        sm: `"nav" "main"`,
+        md: `"nav nav" "side main"`,
+        lg: `"nav nav" "side main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        md: "200px 1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav" bg="blue.100">
+        Nav
+      </GridItem>
+      <GridItem area="side" bg="green.100" hideBelow="md">
+        Side
+      </GridItem>
+      <GridItem area="main" bg="orange.100">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
